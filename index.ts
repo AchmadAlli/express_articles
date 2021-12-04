700d9ed9c9d009cbb7ee6c938774aa467dd3fbbd
Achmad Ali Baidlowi <achmadali.baidlowi@gmail.com>
2021-12-04 11:34:54 +0700

    add request parser

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
-import { Response } from "express"
-
+import Express from 'express'
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.port || 3000
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false})) // middleware to parse x-www-form-urlencoded
 
 app.listen(port, () => {
   console.log(`app running at : http://localhost:${port}`)
