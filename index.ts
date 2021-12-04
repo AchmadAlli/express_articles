d5c7764689c834af01848b917713bf194b2a641b
Achmad Ali Baidlowi <achmadali.baidlowi@gmail.com>
2021-12-04 11:03:57 +0700

    add cors

--- index.ts
+++ index.ts
@@ -2,11 +2,10 @@ import { Response } from "express"
 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.port || 3000
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World')
-})
+app.use(cors())
 
 app.listen(port, () => {
   console.log(`app running at : http://localhost:${port}`)
